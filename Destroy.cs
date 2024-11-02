c39217189f7be79cf98188d4aa1587f1a81a7ecf
werjifer <derlind2010@gmail.com>
2024-11-02 17:57:12 +0500

    1

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //уничтожает объект
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
